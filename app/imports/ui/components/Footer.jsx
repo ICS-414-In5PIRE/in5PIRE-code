40392a5c347d41fc1e8fff7c9bd1f955fdbb7b3c
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2024-09-08 21:04:49 -1000

    adjusted some css. made the footer smaller vertically

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -10,45 +10,44 @@ const Footer = () => {
         <Row>
           {/* Spire LLP Information */}
 
-          <Col md={6} className="footer-col">
+          <Col md={4} className="footer-col">
             <div className="footer-title"><a href="https://www.spirehawaii.com/firm/our-firm" className="link">Spire LLP</a></div>
             <div className="footer-list">
-              <div><a href="spirehawaii.com/firm/our-philosophy" className="link">Our Philosophy</a></div>
-              <div><a href="spirehawaii.com/firm/our-team" className="link">Our Team</a></div>
-              <div><a href="https://www.spirenewyork.com/" className="link">Spire New York</a></div>
+              <div><a href="https://www.spirehawaii.com/firm/our-philosophy" className="link">Our Philosophy</a></div>
+              <div><a href="https://www.spirehawaii.com/firm/our-team" className="link">Our Team</a></div>
+              {/*<div><a href="https://www.spirenewyork.com/" className="link">Spire New York</a></div>*/}
             </div>
             <div className="footer-contact">
               <a href="tel:+8085360066" className="link">(808) 536-0066</a><br />
               <a href="mailto:contactus@spirehi.com" className="link">contactus@spirehi.com</a><br />
-              <address className="address">700 Bishop Street, Suite 2001, Honolulu, HI 96813</address>
+              <div>700 Bishop Street, Suite 2001, Honolulu, HI 96813</div>
             </div>
           </Col>
 
           {/* University Information */}
-          <Col md={6} className="footer-col">
+          <Col md={4} className="footer-col">
             <div className="footer-title"><a href="https://www.ics.hawaii.edu/" className="link">Department of Information and Computer Sciences</a></div>
             <div className="footer-list">
               <div><a href="https://github.com/ICS-414-In5PIRE/in5PIRE.github.io" className="link">Project Home Page</a></div>
               <div className="footer-contact">
                 <div><a href="https://manoa.hawaii.edu/" className="link">University of Hawaii at Manoa</a></div>
-                <address className="address">2500 Campus Road, Honolulu, HI 96822</address>
+                <div>2500 Campus Road, Honolulu, HI 96822</div>
               </div>
 
             </div>
           </Col>
 
           {/* Logos Column */}
-          <Col md={4} className="footer-logos text-center">
-            <img src="/public/images/spirellp.png" alt="Company 1 Logo" className="logo-img" />
-            <Image src="app/public/images/spirellp.png" width="150px" />
-            <img src="/images/icslogo.png" alt="Company 2 Logo" className="logo-img" />
-            <img src="/images/uhmanoaimage.png" alt="Company 3 Logo" className="logo-img" />
+          <Col md={4}className="footer-logos">
+            <img src="/images/spirellp.png" alt="Spire LLP Logo" className="logo-img" />
+            <img src="/images/icslogo.png" alt="ICS Logo" className="logo-img" />
+            <img src="/images/uhmanoaimage.png" alt="UH manoa logo Logo" className="logo-img" />
           </Col>
         </Row>
         <Row className="copyright-section">
           <Col className="text-center">
             <div>© Spire Hawaii LLP 2023</div>
-            <a href="/privacy-policy" className="link">Privacy Policy</a>
+            <a href="https://www.spirehawaii.com/privacy-policy" className="link">Privacy Policy</a>
           </Col>
         </Row>
       </Container>

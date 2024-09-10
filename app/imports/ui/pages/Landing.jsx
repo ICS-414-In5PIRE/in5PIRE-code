b1f25e71c6942e2fe1c499e7b2311dd34b6c4f65
bfd2 <bfd2@hawaii.edu>
2024-09-09 23:47:16 -1000

    Created landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+    <Row className="align-middle text-center py-2">
+      <Col className="d-flex flex-column justify-content-center">
+        <h1 style={{fontSize:"80px"}}>in5PIRE Tool</h1>
+        <p style={{fontSize:"20px"}}>
+          This tool allows users to analyze spreadsheet data and be able
+          to visualize the data you need.
+        </p>
+        <p style={{fontSize:"20px"}}>
+          All in one dashboard.
+        </p>
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
+    </Row>
+    <Row className="align-middle text-center py-2">
+      <Col className="d-grid gap-2">
+        <Button variant="primary" size="lg" href="/signin">Login</Button>
       </Col>
-
+      <Col className="d-grid gap-2">
+        <Button variant="secondary" size="lg" href="#">About Us</Button>
+      </Col>
     </Row>
   </Container>
 );

680827034d37404b571781b8e3d6947589e9dc87
bobbyir <110771308+bobbyir@users.noreply.github.com>
2024-09-11 20:40:23 -1000

    c

--- app/imports/ui/pages/AboutUs.jsx
+++ app/imports/ui/pages/AboutUs.jsx
@@ -10,15 +10,14 @@ const AboutUs = () => {
             <div className="col-12">
               <div className="section-title">
                 <h1 className="display-4 fw-semibold">About Us</h1>
-                <div className="line"></div>  {/* This could be styled as a thin, horizontal rule to visually separate the title from the content */}
-                <p>Hello, we are In5pire. We...</p>  {/* Update content as necessary */}
+                <div className="line"></div>
+                <p>Hello, we are In5pire. We...</p>
               </div>
             </div>
           </div>
           <div className="row justify-content-between align-items-center">
             <div className="col-lg-6">
-              {/* Consider adding 'className="img-fluid"' if you want the image to be responsive */}
-              <Image style={{ height: '450px', width: '550px' }} src="/images/aboutus.png" alt="Landscape" />
+              <Image className="img-fluid" style={{ maxHeight: '450px', maxWidth: '100%' }} src="/images/aboutus.png" alt="About Us landscape" />
             </div>
             <div className="col-lg-5">
               <h1>In5pire</h1>
@@ -29,8 +28,56 @@ const AboutUs = () => {
           </div>
         </div>
       </section>
-    </>
+      <section>
+        <h1 className="title">Our Team</h1>
+        <div className="team-row">
+          <div className="member">
+            <Image src="../../../public/images/Unknown_person.jpg" alt="Ben Smith" roundedCircle />
+            <h2>Bobby Roth</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Jason" roundedCircle />
+            <h2>Brayden Danielson</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Dao Mcgill</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Emily Hsu</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Zachary Stoddard</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Shedrick Klifford Ulibas</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Luke Pagtulingan</h2>
+            <p>Placeholder</p>
+          </div>
+          <div className="member">
+            <Image src="https://miro.medium.com/max/814/1*Ri024ZNn0dtXQDmswflYFw.jpeg" alt="Chris Jones" roundedCircle />
+            <h2>Sean Sunoo</h2>
+            <p>Placeholder</p>
+          </div>
 
+
+        </div>
+
+      </section>
+
+    </>
   );
 }
 

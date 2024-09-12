131f8691bc6e455350088157c30bb8ee3dd54025
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2024-09-11 19:24:08 -1000

    added page

--- app/imports/ui/pages/FinancialProfiles.jsx
+++ app/imports/ui/pages/FinancialProfiles.jsx
@@ -1,66 +1,72 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-import { Stuffs } from '../../api/stuff/StuffCollection';
-import { defineMethod } from '../../api/base/BaseCollection.methods';
-import { PAGE_IDS } from '../utilities/PageIDs';
+import { Container } from 'react-bootstrap';
 
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
-});
+const FinancialProfiles = () => {
+  <Container>
+    <h1>User's Financial Profiles</h1>
+  </Container>
 
-const bridge = new SimpleSchema2Bridge(formSchema);
+  /* Listing the Cards */
+  <Container className="mx-auto px-0 ">
+    <Row className="justify-content-left">
+        <Col className="pb-3 financial-profile-card">
 
-/* Renders the AddStuff page for adding a document. */
-const AddStuff = () => {
-
-  // On submit, insert the data.
-  const submit = (data, formRef) => {
-    const { name, quantity, condition } = data;
-    const owner = Meteor.user().username;
-    const collectionName = Stuffs.getCollectionName();
-    const definitionData = { name, quantity, condition, owner };
-    defineMethod.callPromise({ collectionName, definitionData })
-      .catch(error => swal('Error', error.message, 'error'))
-      .then(() => {
-        swal('Success', 'Item added successfully', 'success');
-        formRef.reset();
-      });
-  };
-
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
-  let fRef = null;
-  return (
-    <Container id={PAGE_IDS.ADD_STUFF} className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center"><h2>Add Stuff</h2></Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
-            <Card>
-              <Card.Body>
-                <TextField name="name" />
-                <NumField name="quantity" decimal={null} />
-                <SelectField name="condition" />
-                <SubmitField value="Submit" />
-                <ErrorsField />
-              </Card.Body>
+          {/*Card 1*/}
+          <Container className="py-3">
+            <Card id="Financial-Card">
+              <CardHeader className="d-flex justify-content-center" id="browse-club-card-name"><h1>{club.name}</h1></CardHeader>
+              <Row>
+                <Col>
+                  <Image src={club.image} className="img-thumbnail m-4 p-4" />
+                </Col>
+                <Col className="d-flex flex-column">
+                  <Row className="px-4 pt-4">
+                    <h2> Club Type</h2>
+                    <p>{club.type}</p>
+                  </Row>
+                  <Row className="px-4">
+                    <h2> Contact Person</h2>
+                    <p>{club.owner}</p>
+                  </Row>
+                  <Row className="px-4">
+                    <h2> Contact email</h2>
+                    <a href={`mailto:${club.ownerMail}`}>{club.ownerMail}</a>
+                  </Row>
+                </Col>
+              </Row>
+              <Row className="px-4">
+                <h2>Club Purpose</h2>
+                <p>{club.description}</p>
+              </Row>
+              {(currentUser) ? (
+                  <Card.Footer>
+                    <Row>
+                      {(isAdmin) ? ''
+                        : (
+                          <Col>
+                            {(isMember) ? (
+                              <Button className="btn-danger" onClick={() => onLeaveClub(club)} id="leave-club-btn">Leave Club</Button>
+                            ) : <Button className="btn-success" onClick={() => onJoinClub(club)} id="join-club-btn">Join Club</Button>}
+                          </Col>
+                        )}
+                      <Col className="d-flex justify-content-end">
+                        {(canEdit) ? (
+                          <Link to={`/${club._id}`}>
+                            <Button id="edit-club-link">Edit Club</Button>
+                          </Link>
+                        ) : ''}
+                      </Col>
+                    </Row>
+                  </Card.Footer>
+                ) :
+                ''}
             </Card>
-          </AutoForm>
+          </Container>
+
         </Col>
-      </Row>
-    </Container>
-  );
+      ))}
+    </Row>
+  </Container>;
 };
 
-export default AddStuff;
+export default FinancialProfiles;

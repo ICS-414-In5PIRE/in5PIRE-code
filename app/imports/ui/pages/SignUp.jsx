8047267eef0445b392c91eff9c3a18bfa7161bcf
skulibas <107680213+skulibas@users.noreply.github.com>
2024-09-17 12:01:44 -1000

    remove react-router-dom multiple imports

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
